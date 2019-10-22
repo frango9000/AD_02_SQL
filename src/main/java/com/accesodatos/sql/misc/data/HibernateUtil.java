66694e1f93fbd51b56587a763426907f779bca74
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-10-22 23:33:14 +0200

    HibernateUtil update

--- src/main/java/com/accesodatos/sql/misc/data/HibernateUtil.java
+++ src/main/java/com/accesodatos/sql/misc/data/HibernateUtil.java
@@ -2,7 +2,9 @@ package com.accesodatos.sql.misc.data;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class HibernateUtil {
 
@@ -11,7 +13,14 @@ public class HibernateUtil {
     private static SessionFactory buildSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure().buildSessionFactory();
+
+            //  legacy code
+            //  return new Configuration().configure().buildSessionFactory();
+
+            // as per hibernate documentation :
+            // https://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html
+            StandardServiceRegistry sr = new StandardServiceRegistryBuilder().configure().build();
+            return new MetadataSources(sr).buildMetadata().buildSessionFactory();
         } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
