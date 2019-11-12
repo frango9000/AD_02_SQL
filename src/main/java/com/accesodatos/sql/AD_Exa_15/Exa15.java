4099a9a8346ff5249898d3f1ba4d5af5f2293761
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2019-11-12 10:54:31 +0100

    exa 15 desmodularizado..............................

--- src/main/java/com/accesodatos/sql/AD_Exa_15/Exa15.java
+++ src/main/java/com/accesodatos/sql/AD_Exa_15/Exa15.java
@@ -2,8 +2,10 @@ package com.accesodatos.sql.AD_Exa_15;
 
 import com.accesodatos.sql.misc.SessionDB;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -23,49 +25,59 @@ public class Exa15 {
     public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException, XMLStreamException {
         //codigo aqui
         HashSet<PlatoGraso> platosGrasos = new HashSet<>();
-        for (Platos plato : Creaplatoss.readPlatos()) {
-            HashMap<String, Integer> composicion = new HashMap<>();
-            if (sessionDB.connect()) {
-                System.out.println(plato.toString());
-                String sql = "select * from composicion where codp = ?";
-                try (PreparedStatement ps = sessionDB.getConn().prepareStatement(sql)) {
-                    ps.setString(1, plato.getCodigop());
-                    ResultSet resultSet = ps.executeQuery();
-                    while (resultSet.next()) {
-                        composicion.put(resultSet.getString(2), resultSet.getInt(3));
-                    }
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            int grasatotal = 0;
-            for (Entry<String, Integer> entry : composicion.entrySet()) {
-                String codc = entry.getKey();
-                int peso = entry.getValue();
-                System.out.println("Codigo componente: " + codc);
+        try {
+            Platos plato;
+            FileInputStream fis = new FileInputStream(Exa15.RES_PATH + "platoss");
+            ObjectInputStream ois = new ObjectInputStream(fis);
 
-                String sql = "select graxa from componentes where codc = ?";
-                int grasa = 0;
-                try (PreparedStatement ps = sessionDB.getConn().prepareStatement(sql)) {
-                    ps.setString(1, codc);
-                    ResultSet resultSet = ps.executeQuery();
-                    while (resultSet.next()) {
-                        grasa = resultSet.getInt(1);
+            while ((plato = (Platos) ois.readObject()) != null) {
+                HashMap<String, Integer> composicion = new HashMap<>();
+                if (sessionDB.connect()) {
+                    System.out.println(plato.toString());
+                    String sql = "select * from composicion where codp = ?";
+                    try (PreparedStatement ps = sessionDB.getConn().prepareStatement(sql)) {
+                        ps.setString(1, plato.getCodigop());
+                        ResultSet resultSet = ps.executeQuery();
+                        while (resultSet.next()) {
+                            composicion.put(resultSet.getString(2), resultSet.getInt(3));
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                     }
-
-                } catch (SQLException e) {
-                    e.printStackTrace();
                 }
-                System.out.println("Grasa cada 100g " + grasa);
-                System.out.println("Peso " + peso);
-                int grasaparcial = ((grasa * peso) / 100);
-                System.out.println("Total de grasa del componente " + grasaparcial);
-                grasatotal += grasaparcial;
+                int grasatotal = 0;
+                for (Entry<String, Integer> entry : composicion.entrySet()) {
+                    String codc = entry.getKey();
+                    int peso = entry.getValue();
+                    System.out.println("Codigo componente: " + codc);
+
+                    String sql = "select graxa from componentes where codc = ?";
+                    int grasa = 0;
+                    try (PreparedStatement ps = sessionDB.getConn().prepareStatement(sql)) {
+                        ps.setString(1, codc);
+                        ResultSet resultSet = ps.executeQuery();
+                        while (resultSet.next()) {
+                            grasa = resultSet.getInt(1);
+                        }
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Grasa cada 100g " + grasa);
+                    System.out.println("Peso " + peso);
+                    int grasaparcial = ((grasa * peso) / 100);
+                    System.out.println("Total de grasa del componente " + grasaparcial);
+                    grasatotal += grasaparcial;
+                }
+                PlatoGraso platoGraso = new PlatoGraso(plato.getCodigop(), plato.getNomep(), grasatotal);
+                platosGrasos.add(platoGraso);
+                System.out.println("Total en grasas del plato: " + grasatotal);
+                System.out.println();
             }
-            PlatoGraso platoGraso = new PlatoGraso(plato.getCodigop(), plato.getNomep(), grasatotal);
-            platosGrasos.add(platoGraso);
-            System.out.println("Total en grasas del plato: " + grasatotal);
-            System.out.println();
+            ois.close();
+            fis.close();
+        } catch (Exception e) {
+            System.out.println("Exception during deserialization: " + e);
         }
         sessionDB.close();
         writePlatosGrasosSAX(platosGrasos);
@@ -98,11 +110,11 @@ public class Exa15 {
 
     private static void writePlatoSAX(PlatoGraso platoGraso, XMLStreamWriter out) throws XMLStreamException {
         out.writeStartElement("plato");
-        out.writeAttribute("codigo", platoGraso.getCodigop());
-        out.writeStartElement("nome");
+        out.writeAttribute("codigop", platoGraso.getCodigop());
+        out.writeStartElement("nomep");
         out.writeCharacters(platoGraso.getNomep());
         out.writeEndElement();
-        out.writeStartElement("graxa");
+        out.writeStartElement("graxatotal");
         out.writeCharacters(Integer.toString(platoGraso.getGrasa()));
         out.writeEndElement();
         out.writeEndElement();
